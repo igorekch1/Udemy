3b9e3c630dc3920a50217c6db675077818760217
igorekch1 <igor.chernyaev1999@gmail.com>
2019-07-20 21:11:31 +0300

    implemented async fetch post and user action creator

--- React/posts/src/actions/index.js
+++ React/posts/src/actions/index.js
@@ -1,8 +1,24 @@
 import jsonPlaceholder from '../api/jsonplaceholder';
+import _ from "lodash";
 
-export const fetchPosts = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
 
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach((id) => dispatch(fetchUser(id)))
+        .value();
+}
+
+const fetchPosts = () => async dispatch => {
     const posts = await jsonPlaceholder.get('/posts');
     
-    dispatch({ type: "FETCH_POSTS", payload: posts });
+    dispatch({ type: "FETCH_POSTS", payload: posts.data });
 } 
+
+const fetchUser = id => async dispatch => {
+    const user = await jsonPlaceholder.get(`/users/${id}`);
+    
+    dispatch({ type: "FETCH_USERS", payload: user.data });
+}
